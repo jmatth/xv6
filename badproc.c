15f3beb73c92bae882d321ef59bd1365596a32a2
Josh Matthews <jmatthews@twilio.com>
2014-09-20 08:58:04 -0400

    Making badproc daemonize and write uptime to file

--- badproc.c
+++ badproc.c
@@ -1,7 +1,21 @@
 #include "user.h"
+#include "fcntl.h"
 
 int main(int argc, char *argv[])
 {
-    printf(1, "Just badproc things\n");
-    exit();
+    int pid;
+    int output;
+    int curruptime;
+
+    if ((pid = fork()) > 0)
+        exit();
+
+    while(1)
+    {
+        curruptime = uptime();
+        output = open("badproc.out", O_CREATE | O_WRONLY);
+        printf(output, "%d\n", curruptime);
+        close(output);
+        sleep(1000);
+    }
 }
