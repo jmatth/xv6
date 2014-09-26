23541c3e5b9da6dbe850cfc4e40574e9da71e623
Josh Matthews <jmatthews@twilio.com>
2014-09-26 15:19:21 -0400

    Implementing static uptime rootkit with sysreplace
    Currently it just replaces the real uptime call with one that always
    returns 2, which is copied into the kernel so it works from any process.
    Needs to be revised to call the original uptime and add 5000 to its
    return value.

--- myuptime.c
+++ myuptime.c
@@ -9,6 +9,24 @@ void dummy()
     return;
 }
 
+int exploitStage2() {
+    return 2;
+}
+
+void* exploitStage1() {
+    char *(*kalloc)(void);
+    char *newpage;
+    /* int oldcall; */
+
+    kalloc = (char*(*)())findkalloc();
+    newpage =kalloc();
+
+    memmove(newpage, &exploitStage2, 1024);
+    /* sysreplace(14, (uint)newpage, (uint)&oldcall); */
+
+    return newpage;
+}
+
 int myuptime(void)
 {
   int u = x();
@@ -19,10 +37,16 @@ int main()
 {
   int u;
   x = 0;
+
   u = uptime();
   printf(1, "%d\n", u);
-  sysreplace(14, (uint) &myuptime, (uint) &x);
+
+  sysreplace(14, (uint) &exploitStage1, (uint) &x);
+  u = uptime();
+  sysreplace(14, (uint) u, (uint) &x);
+
   u = uptime();
   printf(1, "%d\n", u);
+
   exit();
 }
