59aa4c0b578f79d212b69d83e447c106db6f8459
Josh Matthews <jmatthews@twilio.com>
2014-10-04 21:27:11 -0400

    Adding default actions to sigrecieve

--- signal.c
+++ signal.c
@@ -9,6 +9,21 @@
 void
 sigrecieve(int sig, struct trapframe *tf)
 {
+  // Default actions
+  if((int)proc->handlers[sig] == -1)
+  {
+    switch (sig) {
+      case SIGSEGV:
+        cprintf("pid %d %s: caught SIGSEGV, killed. "
+                "eip 0x%x addr 0x%x\n",
+                proc->pid, proc->name, tf->eip, rcr2());
+        proc->killed = 1;
+        break;
+      default:
+        return;
+    }
+  }
+
   // Push the current code location and appropriate handler location onto the
   // process's stack.
   // I'll make my own calling convention, with blackjack and hookers...
