db604c7e50af03dab9e308be638bf6b54344d3d4
Josh Matthews <jmatthews@twilio.com>
2014-10-23 21:37:46 -0400

    Updating context test to check all functions

--- context_test.c
+++ context_test.c
@@ -1,22 +1,21 @@
 #include "user.h"
 #include "context.h"
 
-struct ucontext otherc1, otherc2;
+struct ucontext otherc1, otherc2, mainc;
 
 void padding(){
   return;
 }
 
-void other1() {
-  static int called = 0;
-  printf(1, "In other1\n");
-  if (called < 5) {
+void other1(int num_calls) {
+  int called = 1;
+  printf(1, "num_calls is %d\n", num_calls);
+  while (called <= num_calls) {
+    printf(1, "in other1 %d\n", called);
     called++;
-    setcontext(&otherc2);
-  } else {
-    printf(1, "Exiting in other1\n");
-    exit();
+    swapcontext(&otherc1, &otherc2);
   }
+  setcontext(&mainc);
 }
 
 void other2() {
@@ -26,6 +25,7 @@ void other2() {
 
 int main(int argc, char *argv[])
 {
+  int switched = 0;
   printf(1, "Starting main\n");
 
   otherc1.uc_stack.ss_sp = malloc(512);
@@ -33,9 +33,18 @@ int main(int argc, char *argv[])
   otherc2.uc_stack.ss_sp = malloc(512);
   otherc1.uc_stack.ss_size = 512;
   otherc2.uc_stack.ss_size = 512;
-  makecontext(&otherc1, &other1, 0, 0);
+  makecontext(&otherc1, (void(*)())&other1, 1, 5);
   makecontext(&otherc2, &other2, 0, 0);
 
+  getcontext(&mainc);
+
+  if (switched)
+  {
+    printf(1, "Back in main, exiting\n");
+    exit();
+  }
+
+  switched = 1;
   printf(1, "Setting context to other1\n");
   setcontext(&otherc1);
 
