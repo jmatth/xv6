e27830921b322c406f37f0f73d1f46a40ddb58c2
katiepru <katiepru@nbcs.rutgers.edu>
2014-10-23 21:49:55 -0400

    bugfixes

--- context.c
+++ context.c
@@ -19,7 +19,7 @@ void init_stack(ucontext_t *ucp, int argc, int *argv) {
 int getcontext(ucontext_t *ucp) {
     *ucp = *((ucontext_t *)malloc(sizeof(ucontext_t)));
     asm("\t movl (%%ebp),%0"   : "=r"(ucp->ebp));
-    asm("\t movl -1(%%ebp),%0" : "=r"(ucp->eip));
+    asm("\t movl -4(%%ebp),%0" : "=r"(ucp->eip));
     asm("\t movl %%ebp,%0"     : "=r"(ucp->esp));
     ucp->esp -=4;
     return 0;
@@ -50,18 +50,22 @@ int setcontext(ucontext_t *ucp) {
 }
 
 int swapcontext(ucontext_t *oucp, ucontext_t *ucp) {
+    // Need to store eip for the jump in a reg
+    register uint target;
+    target = (uint)ucp->eip;
+
     // Store our callers esp, ebp, eip
     asm("\t movl (%%ebp),%0"   : "=r"(oucp->ebp));
-    asm("\t movl -1(%%ebp),%0" : "=r"(oucp->eip));
+    asm("\t movl -4(%%ebp),%0" : "=r"(oucp->eip));
     asm("\t movl %%ebp,%0"     : "=r"(oucp->esp));
     oucp->esp -=4;
 
     // Restore esp and ebp from old context
-    asm("\t movl %0, %%ebp" : : "r"(ucp->ebp));
     asm("\t movl %0, %%esp" : : "r"(ucp->esp));
+    asm("\t movl %0, %%ebp" : : "r"(ucp->ebp));
 
     // Ret to continue execution from where we were before
-    asm("jmp %0" : :"r"(ucp->eip));
+    asm("jmp %0" : :"r"(target));
 
     return 1;
 }
