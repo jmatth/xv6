594e53f8718cbc39c3e14c8c861ef4c1c7c9116e
Josh Matthews <jmatthews@twilio.com>
2014-11-21 22:42:50 -0500

    Making testcow comform to spec

--- testcow.c
+++ testcow.c
@@ -7,18 +7,37 @@ main(int argc, char *argv[])
 {
   int i;
   int pid;
-  printf(1, "i is at 0x%x; pid is at 0x%x\n", &i, &pid);
-  pid = cowfork();
-  printf(1, "pid: %d\n", pid);
-  i = 3;
-  if(pid == 0)
-  {
-    i = 5;
-    printf(1, "I think i'm in the child, and i is %d\n", i);
-    exit();
-  }
+  int pre;
+  int post;
 
-  wait();
-  printf(1, "I think i'm in the parent and i is %d\n", i);
+  pre = uptime();
+  for (i = 0; i < 400; i++) {
+    pid = fork();
+    if (pid == 0)
+      exit();
+    if (pid > 0)
+      continue;
+    if (pid < 0)
+      while(wait() >= 0);
+  }
+  post = uptime();
+  while(wait() >= 0);
+
+  printf(1, "400 fork() calls took %d\n", post-pre);
+
+  pre = uptime();
+  for (i = 0; i < 400; i++) {
+    pid = cowfork();
+    if (pid == 0)
+      exit();
+    if (pid > 0)
+      continue;
+    if (pid < 0)
+      while(wait() >= 0);
+  }
+  post = uptime();
+  while(wait() >= 0);
+  printf(1, "400 cowfork() calls took %d\n", post-pre);
+
   exit();
 }
