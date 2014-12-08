da5873c5fc1899e2d5d43b68ac60e733f410c4af
Josh Matthews <jmatthews@twilio.com>
2014-12-07 20:36:13 -0500

    Making bfindmmap work anywhere in the kernel

--- bio.c
+++ bio.c
@@ -110,7 +110,7 @@ bfindmmap(uchar *addr)
  loop:
   // Is the sector already cached?
   for(b = bcache.head.next; b != &bcache.head; b = b->next){
-    if(PGROUNDDOWN((uint)b->data) == (uint)addr && !(b->flags & B_DIRTY)){
+    if(PGROUNDDOWN((uint)b->data) == (uint)addr && !(b->flags & B_DIRTY) && b->flags & B_MMAP){
       if(!(b->flags & B_BUSY)){
         b->flags |= B_BUSY;
         release(&bcache.lock);
